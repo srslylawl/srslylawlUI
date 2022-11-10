889aec28584b423c0e0150b9e09f904fcb3b35da
Andreas <imba@live.at>
2022-11-10 19:37:27 +0100

    version bump 1.35

--- Code.lua
+++ Code.lua
@@ -10,7 +10,7 @@ srslylawlUI = srslylawlUI or {}
 #############################################################
 ]]
 
-local version = "1.34"
+local version = "1.35"
 
 
 srslylawlUI.loadedSettings = {}
